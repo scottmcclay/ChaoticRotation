896837c0855680b0463601746a46390b32454e45
scottmcclay <scott.mcclay.csm@gmail.com>
2017-04-14 12:38:15 -0700

    First working revision with backtracking algorithm

---
+++ ChaoticRotation/Properties/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+﻿//
+// AssemblyInfo.cs
+//
+// Authors:
+//  Scott McClay <scott.mcclay.csm@gmail.com>
+//
+// Copyright (C) 2017 Scott McClay
+//
+// You may use, distribute and modify this code under the terms of the
+// GNU Affero General Public License Verson 3 (GNU AGPL V3).
+// http://www.gnu.org/licenses/agpl.html
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChaoticRotation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChaoticRotation")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Scott McClay")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8193d61-02d4-4542-909c-6f9089950b09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChaoticRotationTests/RaceGeneratorTests.cs
@@ -0,0 +1,93 @@
+﻿//
+// RaceGeneratorTests.cs
+//
+// Authors:
+//  Scott McClay <scott.mcclay.csm@gmail.com>
+//
+// Copyright (C) 2017 Scott McClay
+//
+// You may use, distribute and modify this code under the terms of the
+// GNU Affero General Public License Verson 3 (GNU AGPL V3).
+// http://www.gnu.org/licenses/agpl.html
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ChaoticRotation.Tests
+{
+    [TestFixture()]
+    public class RaceGeneratorTests
+    {
+        [Test, TestCaseSource("TestCases")]
+        public void TestRaceGeneration(int numRacers, int numLanes)
+        {
+            RaceGenerator rg = new RaceGenerator();
+            int[,] races = rg.Solve(numRacers, numLanes);
+
+            // for each racer, make a list of the races they are assigned to, by lane
+            Dictionary<int, int[]> lanesByRacer = new Dictionary<int, int[]>();
+            for (int i = 1; i <= numRacers; i++)
+            {
+                lanesByRacer.Add(i, new int[numLanes]);
+            }
+
+            int numRaces = rg.GetNumRaces(numRacers, numLanes);
+
+            for (int raceNum = 1; raceNum <= numRacers; raceNum++)
+            {
+                for (int laneNum = 1; laneNum <= numLanes; laneNum++)
+                {
+                    int racer = races[raceNum - 1, laneNum - 1];
+                    lanesByRacer[racer][laneNum - 1] = raceNum;
+                }
+            }
+
+            // validate all the assignments
+            foreach (int racer in lanesByRacer.Keys)
+            {
+                // make sure the racer has a chance to run in each lane
+                Assert.That(lanesByRacer[racer], Has.None.EqualTo(0));
+
+                // make sure the racer isn't scheduled for more than one lane in a single race
+                Assert.That(lanesByRacer[racer], Is.Unique);
+
+                // make sure all race assignments are valid
+                Assert.That(lanesByRacer[racer], Has.None.GreaterThan(numRaces));
+            }
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                int minRacers = 2;
+                int maxRacers = 50;
+                int minLanes = 2;
+                int maxLanes = 6;
+
+                for (int numRacers = minRacers; numRacers < maxRacers; numRacers++)
+                {
+                    for (int numLanes = minLanes; numLanes < maxLanes; numLanes++)
+                    {
+                        if (numRacers >= numLanes)
+                        {
+                            yield return new TestCaseData(numRacers, numLanes)
+                                .SetName(string.Format("{0} Racers, {1} Lanes", numRacers.ToString("D2"), numLanes))
+                                .SetCategory(string.Format("{0} Lanes", numLanes));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
